b06c2b2717684e63b9e07ca0a365df823539967f
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-08 17:13:32 -0400

    Through unit testing, created the constructor for a Bill, and created it's first string - name. This will be where we store the name of a Bill. IE: AEP Electric.

--- src/main/java/com/wrthompsonjr/personal/finance/Bill.java
+++ src/main/java/com/wrthompsonjr/personal/finance/Bill.java
@@ -2,4 +2,13 @@ package com.wrthompsonjr.personal.finance;
 
 public class Bill {
 
+    private String name;
+
+    public Bill(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/com/wrthompsonjr/personal/finance/BillTest.java
+++ src/test/java/com/wrthompsonjr/personal/finance/BillTest.java
@@ -3,20 +3,29 @@ package com.wrthompsonjr.personal.finance;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BillTest {
+
+    private static final String NAME = "AEP Electric";
 
     Bill underTest;
 
     @BeforeEach
     public void setUp() {
-        underTest = new Bill();
+        underTest = new Bill(NAME);
     }
 
     @Test
     public void shouldInstantiateABill() {
         assertNotNull(underTest);
+    }
+
+    @Test
+    public void shouldConstructABillAndRetrunABillName() {
+        String check = underTest.getName();
+        assertEquals(NAME, check);
     }
 
 }
