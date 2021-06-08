2ee27568d64d79780d1c37383f6934a3656a3c78
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-08 00:13:01 -0400

    Created and Setup BillTest Class to start Unit Test, as well as created the Bill Class through Unit Testing.

---
+++ src/test/java/com/wrthompsonjr/personal/finance/BillTest.java
@@ -0,0 +1,24 @@
+package com.wrthompsonjr.personal.finance;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class BillTest {
+
+    Bill underTest;
+
+    @BeforeEach
+    public void setUp() {
+        underTest = new Bill();
+    }
+
+    @Test
+    public void shouldInstantiateABill() {
+        assertNotNull(underTest);
+    }
+
+}
