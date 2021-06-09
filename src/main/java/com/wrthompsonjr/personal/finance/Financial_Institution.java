f8448c641c27ec594ab81db756341649e333d121
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-08 22:44:04 -0400

    Through Unit Testing, created the Constructor for the Financial Institution Class and returned a Name for a Financial Institution.

--- src/main/java/com/wrthompsonjr/personal/finance/Financial_Institution.java
+++ src/main/java/com/wrthompsonjr/personal/finance/Financial_Institution.java
@@ -2,5 +2,13 @@ package com.wrthompsonjr.personal.finance;
 
 public class Financial_Institution {
 
+    public String name;
 
+    public Financial_Institution(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/com/wrthompsonjr/personal/finance/Financial_InstitutionTest.java
+++ src/test/java/com/wrthompsonjr/personal/finance/Financial_InstitutionTest.java
@@ -3,18 +3,29 @@ package com.wrthompsonjr.personal.finance;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class Financial_InstitutionTest {
+
+    private static final String NAME = "USAA";
 
     Financial_Institution underTest;
 
     @BeforeEach
     public void setUp() {
-        underTest = new Financial_Institution();
+        underTest = new Financial_Institution(NAME);
     }
 
     @Test
     public void shouldInstantiateAFinancialInstitution() {
+        assertNotNull(underTest);
+    }
 
+    @Test
+    public void shouldConstructAFinancialInstitutionAndReturnAName() {
+        String check = underTest.getName();
+        assertEquals(NAME, check);
     }
 
 }
