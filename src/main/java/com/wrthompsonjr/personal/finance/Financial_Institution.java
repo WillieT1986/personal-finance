40c8cfb96959ba4ceeb1a9593af56781f6a77961
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-08 23:03:22 -0400

    Through Unit Testing, can now return the Financial Institution Type. IE: Is it a Bank, Credit Union, or a Crypto Exchange?

--- src/main/java/com/wrthompsonjr/personal/finance/Financial_Institution.java
+++ src/main/java/com/wrthompsonjr/personal/finance/Financial_Institution.java
@@ -3,12 +3,18 @@ package com.wrthompsonjr.personal.finance;
 public class Financial_Institution {
 
     public String name;
+    public String financial_institution_type;
 
-    public Financial_Institution(String name) {
+    public Financial_Institution(String name, String financial_institution_type) {
         this.name = name;
+        this.financial_institution_type = financial_institution_type;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getFinancialInstitutionType() {
+        return financial_institution_type;
     }
 }
--- src/test/java/com/wrthompsonjr/personal/finance/Financial_InstitutionTest.java
+++ src/test/java/com/wrthompsonjr/personal/finance/Financial_InstitutionTest.java
@@ -9,12 +9,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class Financial_InstitutionTest {
 
     private static final String NAME = "USAA";
+    private static final String FINANCIAL_INSTITUTION_TYPE = "Bank";
 
     Financial_Institution underTest;
 
     @BeforeEach
     public void setUp() {
-        underTest = new Financial_Institution(NAME);
+        underTest = new Financial_Institution(NAME, FINANCIAL_INSTITUTION_TYPE);
     }
 
     @Test
@@ -28,4 +29,9 @@ public class Financial_InstitutionTest {
         assertEquals(NAME, check);
     }
 
+    @Test
+    public void shouldReturnAFinancialInstitutionType() {
+        String check = underTest.getFinancialInstitutionType();
+        assertEquals(FINANCIAL_INSTITUTION_TYPE, check);
+    }
 }
