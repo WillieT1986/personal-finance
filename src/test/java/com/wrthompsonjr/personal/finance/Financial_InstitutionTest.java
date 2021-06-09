9dd47a25ecd22b2106a98f4722a963b4f022d61a
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-08 21:48:33 -0400

    Created the Financial Institution Class through Unit Testing.

---
+++ src/test/java/com/wrthompsonjr/personal/finance/Financial_InstitutionTest.java
@@ -0,0 +1,20 @@
+package com.wrthompsonjr.personal.finance;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Financial_InstitutionTest {
+
+    Financial_Institution underTest;
+
+    @BeforeEach
+    public void setUp() {
+        underTest = new Financial_Institution();
+    }
+
+    @Test
+    public void shouldInstantiateAFinancialInstitution() {
+
+    }
+
+}
