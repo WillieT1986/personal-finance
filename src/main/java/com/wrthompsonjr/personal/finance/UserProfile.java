1347550e5d9a9299d917c31aa562a17aff0d169c
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-11 18:07:32 -0400

    Through Unit Testing, created the constructor and the First, Middle, Last, Suffix names. Did them all at once since they're all apart of the name and to save space after thinking about it.

--- src/main/java/com/wrthompsonjr/personal/finance/UserProfile.java
+++ src/main/java/com/wrthompsonjr/personal/finance/UserProfile.java
@@ -2,5 +2,31 @@ package com.wrthompsonjr.personal.finance;
 
 public class UserProfile {
 
+    public String firstName;
+    public String middleName;
+    public String lastName;
+    public String suffix;
 
+    public UserProfile(String firstName, String middleName, String lastName, String suffix) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.suffix = suffix;
+    }
+
+    public String getUserFirstName() {
+        return firstName;
+    }
+
+    public String getUserMiddleName() {
+        return middleName;
+    }
+
+    public String getUserLastName() {
+        return lastName;
+    }
+
+    public String getUserSuffix() {
+        return suffix;
+    }
 }
--- src/test/java/com/wrthompsonjr/personal/finance/UserProfileTest.java
+++ src/test/java/com/wrthompsonjr/personal/finance/UserProfileTest.java
@@ -3,15 +3,21 @@ package com.wrthompsonjr.personal.finance;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UserProfileTest {
+
+    private static final String FIRST_NAME = "First";
+    private static final String MIDDLE_NAME = "Middle";
+    private static final String LAST_NAME = "Last";
+    private static final String SUFFIX = "II";
 
     UserProfile underTest;
 
     @BeforeEach
     public void setUp() {
-        underTest = new UserProfile();
+        underTest = new UserProfile(FIRST_NAME, MIDDLE_NAME, LAST_NAME, SUFFIX);
     }
 
     @Test
@@ -19,4 +25,15 @@ public class UserProfileTest {
         assertNotNull(underTest);
     }
 
+    @Test
+    public void shouldReturnAUsersFirstNameMiddleNameLastNameAndSuffix() {
+        String check_firstName = underTest.getUserFirstName();
+        String check_middleName = underTest.getUserMiddleName();
+        String check_lastName = underTest.getUserLastName();
+        String check_suffix = underTest.getUserSuffix();
+        assertEquals(FIRST_NAME, check_firstName);
+        assertEquals(MIDDLE_NAME, check_middleName);
+        assertEquals(LAST_NAME, check_lastName);
+        assertEquals(SUFFIX, check_suffix);
+    }
 }
