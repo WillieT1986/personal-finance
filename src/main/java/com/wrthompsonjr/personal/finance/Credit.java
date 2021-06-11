3f507a13c7c68174a6f0f72277b5a8f387bb8690
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-11 17:34:12 -0400

    Through Unit Testing, Added Credit a Receive Date & Due Date for the Credit Class.

--- src/main/java/com/wrthompsonjr/personal/finance/Credit.java
+++ src/main/java/com/wrthompsonjr/personal/finance/Credit.java
@@ -6,12 +6,16 @@ public class Credit {
     public String creditType;
     public Double creditAmount;
     public String creditIssuer;
+    public String creditDateReceived;
+    public String creditDueDate;
 
-    public Credit(String creditName, String creditType, Double creditAmount, String creditIssuer) {
+    public Credit(String creditName, String creditType, Double creditAmount, String creditIssuer, String creditDateReceived, String creditDueDate) {
         this.creditName = creditName;
         this.creditType = creditType;
         this.creditAmount = creditAmount;
         this.creditIssuer = creditIssuer;
+        this.creditDateReceived = creditDateReceived;
+        this.creditDueDate = creditDueDate;
     }
 
     public String getCreditName() {
@@ -28,5 +32,13 @@ public class Credit {
 
     public String getCreditIssuer() {
         return creditIssuer;
+    }
+
+    public String getCreditReceivedDate() {
+        return creditDateReceived;
+    }
+
+    public String getCreditDueDate() {
+        return creditDueDate;
     }
 }
--- src/test/java/com/wrthompsonjr/personal/finance/CreditTest.java
+++ src/test/java/com/wrthompsonjr/personal/finance/CreditTest.java
@@ -3,8 +3,7 @@ package com.wrthompsonjr.personal.finance;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CreditTest {
 
@@ -12,12 +11,14 @@ public class CreditTest {
     private static final String CREDIT_TYPE = "Credit Card";
     private static final Double CREDIT_AMOUNT = 500.00;
     private static final String CREDIT_ISSUER = "Chase Bank";
+    private static final String CREDIT_DATE_RECEIVED = "01/01/2021";
+    private static final String CREDIT_DUE_DATE = "01/15/2021";
 
     Credit underTest;
 
     @BeforeEach
     public void setUp() {
-        underTest = new Credit(CREDIT_NAME, CREDIT_TYPE, CREDIT_AMOUNT, CREDIT_ISSUER);
+        underTest = new Credit(CREDIT_NAME, CREDIT_TYPE, CREDIT_AMOUNT, CREDIT_ISSUER, CREDIT_DATE_RECEIVED, CREDIT_DUE_DATE);
     }
 
     @Test
@@ -47,5 +48,17 @@ public class CreditTest {
     public void shouldReturnCreditIssuer() {
         String check = underTest.getCreditIssuer();
         assertEquals(CREDIT_ISSUER, check);
+    }
+
+    @Test
+    public void shouldReturnCreditReceivedDate() {
+        String check = underTest.getCreditReceivedDate();
+        assertEquals(CREDIT_DATE_RECEIVED, check);
+    }
+
+    @Test
+    public void shouldReturnCreditDueDate() {
+        String check = underTest.getCreditDueDate();
+        assertEquals(CREDIT_DUE_DATE, check);
     }
 }
