a7330534adb52496f0762c124f4aaf7a0a3cc7e5
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-10 23:41:16 -0400

    Through unit testing, created the Double for Amount. This is where the User will add their Amount earned.

--- src/main/java/com/wrthompsonjr/personal/finance/Income.java
+++ src/main/java/com/wrthompsonjr/personal/finance/Income.java
@@ -3,12 +3,18 @@ package com.wrthompsonjr.personal.finance;
 public class Income {
 
     public String source;
+    private Double amount;
 
-    public Income(String source) {
+    public Income(String source, Double amount) {
         this.source = source;
+        this.amount = amount;
     }
 
     public String getSourceOfIncome() {
         return source;
+    }
+
+    public Double getIncomeAmount() {
+        return amount;
     }
 }
--- src/test/java/com/wrthompsonjr/personal/finance/IncomeTest.java
+++ src/test/java/com/wrthompsonjr/personal/finance/IncomeTest.java
@@ -7,17 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IncomeTest {
     private static final String SOURCE = "Work";
+    private static final Double AMOUNT = 2500.00;
+
     Income underTest;
 
     @BeforeEach
     public void setUp() {
-        underTest = new Income(SOURCE);
+        underTest = new Income(SOURCE, AMOUNT);
     }
 
     @Test
     public void shouldReturnTheSource() {
         String check = underTest.getSourceOfIncome();
         assertEquals(SOURCE, check);
+    }
 
+    @Test
+    public void shouldReturnTheAmountEarned() {
+        Double check = underTest.getIncomeAmount();
+        assertEquals(AMOUNT, check);
     }
 }
