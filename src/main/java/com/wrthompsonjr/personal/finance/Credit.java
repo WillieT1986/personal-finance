aba22de2a753c930d94302b89023d3659e198bcb
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-11 17:07:39 -0400

    Through Unit Testing, added Credit Type

--- src/main/java/com/wrthompsonjr/personal/finance/Credit.java
+++ src/main/java/com/wrthompsonjr/personal/finance/Credit.java
@@ -3,12 +3,18 @@ package com.wrthompsonjr.personal.finance;
 public class Credit {
 
     public String creditName;
+    public String creditType;
 
-    public Credit(String creditName) {
+    public Credit(String creditName, String creditType) {
         this.creditName = creditName;
+        this.creditType = creditType;
     }
 
     public String getCreditName() {
         return creditName;
+    }
+
+    public String getCreditType() {
+        return creditType;
     }
 }
--- src/test/java/com/wrthompsonjr/personal/finance/CreditTest.java
+++ src/test/java/com/wrthompsonjr/personal/finance/CreditTest.java
@@ -9,12 +9,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class CreditTest {
 
     private static final String CREDIT_NAME = "United Explorers Credit Card";
+    private static final String CREDIT_TYPE = "Credit Card";
 
     Credit underTest;
 
     @BeforeEach
     public void setUp() {
-        underTest = new Credit(CREDIT_NAME);
+        underTest = new Credit(CREDIT_NAME, CREDIT_TYPE);
     }
 
     @Test
@@ -26,5 +27,11 @@ public class CreditTest {
     public void shouldReturnNameOfCreditCard() {
         String check = underTest.getCreditName();
         assertEquals(CREDIT_NAME, check);
+    }
+
+    @Test
+    public void shouldReturnCreditType() {
+        String check = underTest.getCreditType();
+        assertEquals(CREDIT_TYPE, check);
     }
 }
