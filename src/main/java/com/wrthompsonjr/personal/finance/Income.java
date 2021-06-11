08c0a14f43dfc89969711d10f82737605be15875
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-11 13:38:16 -0400

    Added a description to the Income Class.

--- src/main/java/com/wrthompsonjr/personal/finance/Income.java
+++ src/main/java/com/wrthompsonjr/personal/finance/Income.java
@@ -1,4 +1,11 @@
 package com.wrthompsonjr.personal.finance;
+/*
+    -- The Income Class --
+    A user will usually have only one source of income but for those who have multiple sources, or
+    share/split their income with a significant other should be tracked to.
+    Example: User has 2x Sources of Income while possibly having access, or balancing the income
+    for another user as well.
+*/
 
 public class Income {
 
