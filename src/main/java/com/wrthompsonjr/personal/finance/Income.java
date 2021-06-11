920b6fad90a52c13c2627eb708c9a18572cb2f59
WillieT1986 <WRThompsonJr86@Gmail.com>
2021-06-10 23:36:39 -0400

    Through unit testing, created the constructor for Income, and added the String for source which will be the source of income. IE: Work, Benefits, etc...

--- src/main/java/com/wrthompsonjr/personal/finance/Income.java
+++ src/main/java/com/wrthompsonjr/personal/finance/Income.java
@@ -1,4 +1,14 @@
 package com.wrthompsonjr.personal.finance;
 
 public class Income {
+
+    public String source;
+
+    public Income(String source) {
+        this.source = source;
+    }
+
+    public String getSourceOfIncome() {
+        return source;
+    }
 }
--- src/test/java/com/wrthompsonjr/personal/finance/IncomeTest.java
+++ src/test/java/com/wrthompsonjr/personal/finance/IncomeTest.java
@@ -1,13 +1,23 @@
 package com.wrthompsonjr.personal.finance;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class IncomeTest {
-
+    private static final String SOURCE = "Work";
     Income underTest;
 
     @BeforeEach
     public void setUp() {
-        underTest = new Income();
+        underTest = new Income(SOURCE);
+    }
+
+    @Test
+    public void shouldReturnTheSource() {
+        String check = underTest.getSourceOfIncome();
+        assertEquals(SOURCE, check);
+
     }
 }
