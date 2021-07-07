847b1de8c847d245caffaf82fac7669dd725f359
Willie <wrthompsonjr86@gmail.com>
2021-07-06 23:14:06 -0400

    Removing All Files Associated With Credit Class & Financial Institution Class.

--- src/main/java/com/wrthompsonjr/personal/finance/CreditRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wrthompsonjr.personal.finance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import javax.annotation.Resource;
-
-@Resource
-public interface CreditRepository extends JpaRepository<Credit, Long> {
-}
--- src/main/java/com/wrthompsonjr/personal/finance/Financial_Institution_Repository.java
+++
@@ -1,9 +0,0 @@
-package com.wrthompsonjr.personal.finance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import javax.annotation.Resource;
-
-@Resource
-public interface Financial_Institution_Repository extends JpaRepository<Financial_Institution, Long> {
-}
